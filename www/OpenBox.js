ff917b64830b97ae972f63cde72646ce7b3bce35
nuote <1240594088@qq.com>
2017-07-19 17:16:46 +0800

    Update OpenBox.js

--- www/OpenBox.js
+++ www/OpenBox.js
@@ -1,9 +1,15 @@
-
 var exec = require('cordova/exec');
 
-exports.openBox = function(success, error) {
-    exec(success, error, "YouxiBox", "openBox", []);
-};
+//exports.openBox = function(success, error) {
+//    exec(success, error, "YouxiBox", "openBox", []);
+//};
+ 
+    function YouxiBox(){};
+    YouxiBox.prototype.openBox = function(key,success, error){
+        exec(success, error, "YouxiBox", "openBox", [key]);
+    };
+    var YouxiBox = new YouxiBox();
+    module.exports = YouxiBox;
 
 
 
